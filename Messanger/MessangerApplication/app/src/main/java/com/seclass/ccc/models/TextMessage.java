98a71fb690ef90a933e9b776070b78639ec42da4
unknown <dlsrks1021@gmail.com>
2018-06-04 06:12:31 +0900

    Update environment of messenger

---
+++ Messanger/MessangerApplication/app/src/main/java/com/seclass/ccc/models/TextMessage.java
@@ -0,0 +1,8 @@
+package com.seclass.ccc.models;
+
+import lombok.Data;
+
+@Data
+public class TextMessage extends Message{
+    private String messageText;
+}
--- Messanger/MessangerApplication/app/src/test/java/com/seclass/ccc/ExampleUnitTest.java
+++ Messanger/MessangerApplication/app/src/test/java/com/seclass/ccc/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.nimnim.messangerapplication;
+package com.seclass.ccc;
 
 import org.junit.Test;
 
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
+    public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
 }
